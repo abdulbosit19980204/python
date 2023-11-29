b7f39adb6e22469b695a3acc674740828c6a9651
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-30 02:18:56 +0500

    Python lessons

--- pw3school/pyhton_variables/global_output.py
+++ pw3school/pyhton_variables/global_output.py
@@ -1,25 +1,2 @@
-# print("  2")
-# print(" 222 ")
-# print("22222")
-#
-# print("  2\n 222\n22222" )
 
-# a=input("a ni kiriting a=> ")
-# print("   "+a+"\n"," "+a+a+a+"\n",a+a+a+a+a)
-#
-
-# yosh = input("yoshingiz: ")
-# print(2023-int(yosh))
-
-karra = input("karrani kiriting: ")
-son = int(karra)
-print(karra+"*1","=",son*1)
-print(karra+"*2","=",son*2)
-print(karra+"*3","=",son*3)
-print(karra+"*4","=",son*4)
-print(karra+"*5","=",son*5)
-print(karra+"*6","=",son*6)
-print(karra+"*7","=",son*7)
-print(karra+"*8","=",son*8)
-print(karra+"*9","=",son*9)
 
