5249340b178b8ac7b032e542cea1fbe1f9defa39
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-30 02:47:19 +0500

    Global var

--- pw3school/pyhton_variables/global_output.py
+++ pw3school/pyhton_variables/global_output.py
@@ -1,2 +1,28 @@
+#GLOBAL VARIABLES
 
+# Variables that are created outside of a  function (as in all of the examples above) are known as global variables
+# Global variables can be used by everyone, both inside and outside of the function
+
+x="awesome"
+def myFunc():
+    print("Python is "+x)
+
+myFunc()
+
+# If we create a variable the same name inside a function, this variable will be local, and can only be used inside the function
+
+def newFunc():
+    x="running"
+    print("New function is "+x)
+newFunc()
+
+# To create a global variable inside a function, you can use the "global" keyword
+
+def globalVarFunc():
+    global x
+    x="Hello"
+    print(x+ " world")
+
+globalVarFunc()
+print(x+" dunyo")
 
