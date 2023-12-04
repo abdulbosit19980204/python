1a3475739ebb90630bfd8360044f54e513c78b97
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-04 10:32:07 +0500

    Casting

---
+++ python_casting/casting.py
@@ -0,0 +1,14 @@
+#Specify a Variable Type
+
+# There may be times when you want to specify a type on to a variable. This can be done with casting
+# Casting in python is therefore done using constructor functions
+
+# int() - constructs an integer number from an integral, a float literal (by removing  all decimals)
+# or a string literal (providing the string represents a whole number)
+
+# float()
+# str()
+
+x=3
+print(float(x))
+print(str(x))
