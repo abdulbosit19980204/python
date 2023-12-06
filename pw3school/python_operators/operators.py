833df23855458b9df9729413c1c20b27e4221b38
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-06 11:58:35 +0500

    Operators

---
+++ pw3school/python_operators/operators.py
@@ -0,0 +1,73 @@
+# Python Operators
+
+# Operators are used to perform operations on variables and values
+
+print(5 + 3)
+
+# Arithmetic Operators
+
+# +  => Addition
+# - => Subtraction
+# * => Multiplication
+# / => Division
+# % => Modulus
+# ** => Exponentiation
+# // => Floor divison
+
+# Assignment Operators
+
+# =
+# +=
+# -=
+# *=
+# /=
+# **=
+
+# Comparision Operators
+
+# ==
+# !=
+# >
+# <
+# >=
+# <=
+
+# Logical Operators
+
+# and
+# or
+# not
+
+# Python Identity Operators
+
+"""
+Identity operators are used to compare the objects, not if they are equal,  but if they are actual the same
+object, with the same memory location
+"""
+
+# is
+# is not
+x = ["Hello world"]
+y = ["Hello world"]
+z = x
+
+print(x is y)  # False
+print(x is z)  # True
+
+# Membership operators
+
+# Membership operators are used to test if a squence is presented in an object
+
+# in
+# not in
+
+# Bitwise Operators
+
+# Bitwise operators are used to compare (binary) numbers:
+# & - and
+# | - or
+# ^ - xor
+# ~ - not
+# << - Zero fill left shift
+# >> - signed right shift
+
