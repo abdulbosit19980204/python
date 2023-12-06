a6c6d7f731f2e97a83f5101e82ec9c27c2b13e24
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-06 10:55:20 +0500

    Boolean

---
+++ pw3school/python_booleans/booleans.py
@@ -0,0 +1,16 @@
+# Boolean Values
+
+# In programming we often need to know if an expression is True or False
+
+print(10 > 9)  # True
+print(10 < 9)  # False
+print(10 == 9)  # False
+
+# When you run a condition in an if statement, Python returns True or False
+a = 202
+b = 50
+
+if a>b:
+    print("Max = a=> ",a) #Max = a=>  202
+else:
+    print("Max=b=> ",b)
