65a82f6f30f6108f741bf075c4b43f2c3a538620
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-07 20:09:42 +0500

    Juft toq

--- ba.uz/5-dars/5-lesson.py
+++ ba.uz/5-dars/5-lesson.py
@@ -1,5 +1,21 @@
-text = "1 2 3 4 664 112 89 43 -12"
+# text = "1 2 3 4 664 112 89 43 -12"
 
-lst = [int(i) for i in text.split()]
-print(lst)
-print(sum(lst))
+# lst = [int(i) for i in text.split()]
+# print(lst)
+# print(sum(lst))
+
+
+"""Next"""
+
+n = int(input())
+toq=[]
+juft = []
+a  =[int(input()) for _ in range(n)]
+for i in a:
+    if i%2==0:
+        juft.append(i)
+    else:
+        toq.append(i)
+
+print("Juft => ", juft)
+print("Toq => ",toq)
