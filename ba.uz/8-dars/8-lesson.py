8a4e5a1efb79d83deb2e5e82d3dc06355d7777de
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-12 19:31:42 +0500

    Lambda function

---
+++ ba.uz/8-dars/8-lesson.py
@@ -0,0 +1,13 @@
+# def func(a,b,c):
+#     return a*b-c
+#
+# # def add(a, b):
+# #     return a + b
+# #
+#
+# add = lambda x, y: x + y
+# print(add(int(input()), 2))
+
+
+equal = lambda a,b,c: a*b-c
+print(equal(1,2,3))
