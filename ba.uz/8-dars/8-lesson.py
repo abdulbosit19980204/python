a3fb88b2633a408152e008cb8bc78d9cf0af7a78
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-12 20:35:54 +0500

    recursiya

--- ba.uz/8-dars/8-lesson.py
+++ ba.uz/8-dars/8-lesson.py
@@ -8,6 +8,50 @@
 # add = lambda x, y: x + y
 # print(add(int(input()), 2))
 
+# Lambda function
+#
+# equal = lambda a, b, c: a * b - c
+# print(equal(1, 2, 3))
 
-equal = lambda a,b,c: a*b-c
-print(equal(1,2,3))
+
+# Reqursiya
+
+# def pr_numbers(n):
+#     if n >= 1:
+#         pr_numbers(n - 1)
+#
+#     print(n)
+#
+#
+# pr_numbers(10)
+#
+# # recursion, lambda, date, f string
+
+# def even_numbers(n):
+#    if n%2==0:
+#        # print(n, end=" ")
+#        if n >= 2:
+#            even_numbers(n - 2)
+#        print(n, end=" ")
+#    else:
+#        # print(n, end=" ")
+#        if n >= 2:
+#            even_numbers(n - 1)
+#        print(n, end=" ")
+
+def even_numbers(n):
+    if n > 1:
+        if n % 2 == 0:
+            print(n, end=" ")
+        even_numbers(n - 1)
+
+
+even_numbers(int(input()))
+
+# def odd_numbers(n):
+#     print(n)
+#     if n >= 1:
+#         odd_numbers(n - 1)
+#     print(n)
+#
+# odd_numbers(10)
