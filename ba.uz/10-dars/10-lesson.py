f1ea3a8441619fd269d6f47e2c4dbf6a47640581
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-16 19:53:11 +0500

    dictionaries

--- ba.uz/10-dars/10-lesson.py
+++ ba.uz/10-dars/10-lesson.py
@@ -1,3 +1,33 @@
+# d = {
+#     "fakultet": "Filologiya",
+#     "students": [
+#         {
+#             "first_name": "Ali",
+#             "avg_mark": 4.3
+#         },
+#         {
+#             "first_name": "Vali",
+#             "avg_mark": 5.4
+#         },
+#         {
+#             "first_name": "G'ani",
+#             "avg_mark": 4.6
+#         },
+#         {
+#             "first_name": "Sherali",
+#             "avg_mark": 8
+#         }
+#     ]
+# }
+#
+# # print(d["students"][0]["first_name"])
+# marks = [d["students"][i]["avg_mark"] for i in range(len(d["students"]))]
+# info = [d["students"][i] for i in range(len(d["students"]))]
+#
+# maxRate =marks.index(max(marks))
+# print(info[maxRate])
+
+
 d = {
     "fakultet": "Filologiya",
     "students": [
@@ -7,22 +37,30 @@ d = {
         },
         {
             "first_name": "Vali",
-            "avg_mark": 5.4
+            "avg_mark": 3.4
         },
         {
             "first_name": "G'ani",
             "avg_mark": 4.6
         },
         {
-            "first_name": "Sherali",
-            "avg_mark": 8
+            "first_name": "Damir",
+            "avg_mark": 3.2
+        },
+        {
+            "first_name": "Abbos",
+            "avg_mark": 3.1
         }
     ]
 }
 
-# print(d["students"][0]["first_name"])
-marks = [d["students"][i]["avg_mark"] for i in range(len(d["students"]))]
-info = [d["students"][i] for i in range(len(d["students"]))]
+students = d["students"]
+student_mark = 0
+student_name = ""
 
-maxRate =marks.index(max(marks))
-print(info[maxRate])
+for student in students:
+    if student_mark < student["avg_mark"]:
+        student_name = student["first_name"]
+        student_mark = student["avg_mark"]
+
+print(student_name, student_mark)
