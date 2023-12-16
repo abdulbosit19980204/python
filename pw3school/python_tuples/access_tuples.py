bc21f9ed2f0e60c135438be741432ca3760747dd
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-16 16:12:46 +0500

    Access tuples

---
+++ pw3school/python_tuples/access_tuples.py
@@ -0,0 +1,10 @@
+# Access Tuple Items
+# You can access tuple items by referring to the index number, inside square brackets
+
+thisTuple = (1, 2, 3, 4, 5)
+print(thisTuple[2])#3
+
+# Negative indexing
+# Negative indexing means start from the end.
+
+# -1 refers to the last item, -2 refers to the second lst item etc.
