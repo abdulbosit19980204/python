29299072dbab12f004e5308e5dfe158d3658a409
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-18 00:36:34 +0500

    Sets

---
+++ pw3school/python_sets/sets.py
@@ -0,0 +1,11 @@
+# Python Sets
+
+
+# Set
+# Set are used to store multiple items in a single variable.
+# A set is a collections which is unordred, unchangable*, and unindexed
+# * Set items are unchangable, but you can remove items and add new items
+
+# Sets are written with curly brackets
+myset ={"apple","banana","pear","orange"}
+print(myset)#{'banana', 'pear', 'apple', 'orange'}
