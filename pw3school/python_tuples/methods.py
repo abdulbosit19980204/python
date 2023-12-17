0fecf0951308b7f9b3233b915398170aa9dd3e4d
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-17 11:26:53 +0500

    Tuple methods

---
+++ pw3school/python_tuples/methods.py
@@ -0,0 +1,6 @@
+# Tuple Methods
+# Python has two built-in methods that you can use on tuples
+
+# count() - Returns the numbers of times a specified value occurs in a tuple
+# index() - Searches the tuple for a specified value and returns the position of where it was found
+
