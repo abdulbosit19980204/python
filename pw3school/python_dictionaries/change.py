2c4da0ae6b74976ffa03f3610082c914230e5f15
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-19 18:14:19 +0500

    change

---
+++ pw3school/python_dictionaries/change.py
@@ -0,0 +1,20 @@
+# Change Values
+
+# You can change the value of a specific item by referring to its key name
+# change the "age"
+thisDict = {
+    "firstName": "John",
+    "lastName": "Doe",
+    "age": 25
+}
+
+thisDict["age"] = 35
+print(thisDict)  # {'firstName': 'John', 'lastName': 'Doe', 'age': 35}
+
+# Update Dictionary
+# The update() method will update the dictionary with the items from given argument
+# The argument must be a dictionary, or an iterable object with key:value pairs
+# Update the "firstName" of the Dict by using the update() method
+
+thisDict.update({"firstName": "Jeck"})
+print(thisDict) #{'firstName': 'Jeck', 'lastName': 'Doe', 'age': 35}
