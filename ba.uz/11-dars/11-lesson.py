1dbafab27e277f2835a0f07149fe444f95617135
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-19 20:02:39 +0500

    sort by divison number

--- ba.uz/11-dars/11-lesson.py
+++ ba.uz/11-dars/11-lesson.py
@@ -10,19 +10,21 @@
 # print(lst)
 i = 1
 s = 0
-def cmp(x):
-    if x % 2 == 0:
-        return -x
-    else:
-        return x
+def cmp(n):
+    s = 0
+    for j in range(1, n + 1):
+        if n % j == 0:
+            s += 1
+    return s
+
 
 
 lst = [5, 12, 85, 23, 34, 78]
-# lst.sort(key=cmp)
-# print(lst)
-for n in lst:
-    s=0
-    for j in range(1, n+1):
-        if n % j == 0:
-            s += 1
-    print(n, s)
+lst.sort(key=cmp)
+print(lst)
+# for n in lst:
+#     s=0
+#     for j in range(1, n+1):
+#         if n % j == 0:
+#             s += 1
+#     print(n, s)
