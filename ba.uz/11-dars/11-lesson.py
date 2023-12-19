2e0eea1be809f6bb586588721e66098c92f26875
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-19 20:10:12 +0500

    sort by word's len

--- ba.uz/11-dars/11-lesson.py
+++ ba.uz/11-dars/11-lesson.py
@@ -8,20 +8,19 @@
 # lst = [5, 12, 85, 23, 34, 78]
 # lst.sort(key=cmp)
 # print(lst)
-i = 1
-s = 0
-def cmp(n):
-    s = 0
-    for j in range(1, n + 1):
-        if n % j == 0:
-            s += 1
-    return s
 
 
-
-lst = [5, 12, 85, 23, 34, 78]
-lst.sort(key=cmp)
-print(lst)
+# def cmp(n):
+#     s = 0
+#     for j in range(1, n + 1):
+#         if n % j == 0:
+#             s += 1
+#     return s
+#
+#
+# lst = [5, 12, 85, 23, 34, 78]
+# lst.sort(key=cmp)
+# print(lst)
 
 # for n in lst:
 #     s=0
@@ -29,3 +28,15 @@ print(lst)
 #         if n % j == 0:
 #             s += 1
 #     print(n, s)
+
+
+word_list = ["apple", "banana", "nasjdn", "sndjknsdjfn", "ojdjna asdjnajosnd aksdias", "jdjnja asskmas"]
+
+
+def cmp(x):
+    return -len(x)
+
+
+word_list.sort(key=cmp)
+print(word_list)
+
