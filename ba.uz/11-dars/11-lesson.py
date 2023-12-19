1a37bb7a0e71fbe72e305ecc3315e99ba0f5ae44
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-19 19:59:57 +0500

    boluvchilar soni

---
+++ ba.uz/11-dars/11-lesson.py
@@ -0,0 +1,28 @@
+# def cmp(x):
+#     if x % 2 == 0:
+#         return -x
+#     else:
+#         return x
+#
+#
+# lst = [5, 12, 85, 23, 34, 78]
+# lst.sort(key=cmp)
+# print(lst)
+i = 1
+s = 0
+def cmp(x):
+    if x % 2 == 0:
+        return -x
+    else:
+        return x
+
+
+lst = [5, 12, 85, 23, 34, 78]
+# lst.sort(key=cmp)
+# print(lst)
+for n in lst:
+    s=0
+    for j in range(1, n+1):
+        if n % j == 0:
+            s += 1
+    print(n, s)
---
+++ pw3school/python_dictionaries/nested.py
@@ -0,0 +1,5 @@
+# Nested Dictionaries
+
+# A dictionary can contain dictionaries, this is called nested dictionaries
+
+# Creat a dictionary that contain  dicitonaries
