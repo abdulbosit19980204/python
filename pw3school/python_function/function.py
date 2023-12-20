ed45ea1af73350d46241d09b68a52a9b85d9137e
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-20 11:55:03 +0500

    Recursion

--- pw3school/python_function/function.py
+++ pw3school/python_function/function.py
@@ -125,3 +125,15 @@ print("Your order will be ", my_food_price(5, 30000))  # Your order will be  150
 def passFunc():
     pass
 
+# Recursion
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(5)
+
