5c640650215f22e21fac73faa412494dacbdc47d
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-23 20:54:16 +0500

    Telegram bot chat ID

--- ba.uz/13-dars/telegram_bot.py
+++ ba.uz/13-dars/telegram_bot.py
@@ -1,9 +1,11 @@
 import requests
 
 API_TOKEN = '6189703946:AAGb1TA2yDg-sdu0c_AIDn39_07AuzvlZgE'
-CHAT_ID = 1209619850
+# CHAT_ID = 1209619850
 # API_URL = 'https://api.telegram.org/bot{}/getMe'.format(API_TOKEN)
-# API_URL = 'https://api.telegram.org/bot{}/getUpdates'.format(API_TOKEN)
+API_URL = 'https://api.telegram.org/bot{}/getUpdates'.format(API_TOKEN)
+respose = requests.get(API_URL)
+CHAT_ID = respose.json()['result'][0]['message']['chat']['id']
 API_URL = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text=ishlar yaxshimi?'.format(API_TOKEN, CHAT_ID)
 respose = requests.get(API_URL)
 print(respose.json())
@@ -19,6 +21,18 @@ print(respose.json())
  'message': {'message_id': 136, 'from':
   {'id': 1209619850, 'is_bot': False, 'first_name': "To'ychiyev Abdulbosit (عبد الباسط)",
    'username': 'abdulbosit_tatu', 'language_code': 'en'},
-    'chat': {'id': 1209619850, 'first_name': "To'ychiyev Abdulbosit (عبد الباسط)", 'username': 'abdulbosit_tatu', 'type': 'private'}, 'date': 1703346079, 'text': '/start', 'entities': [{'offset': 0, 'length': 6, 'type': 'bot_command'}]}}, {'update_id': 389398453, 'message': {'message_id': 137, 'from': {'id': 1209619850, 'is_bot': False, 'first_name': "To'ychiyev Abdulbosit (عبد الباسط)", 'username': 'abdulbosit_tatu', 'language_code': 'en'}, 'chat': {'id': 1209619850, 'first_name': "To'ychiyev Abdulbosit (عبد الباسط)", 'username': 'abdulbosit_tatu', 'type': 'private'}, 'date': 1703346082, 'text': 'hello'}}]}
+    'chat': {'id': 1209619850, 'first_name': 
+    "To'ychiyev Abdulbosit (عبد الباسط)", 
+    'username': 'abdulbosit_tatu', 'type': 'private'}, 
+    'date': 1703346079, 'text': '/start', 
+    'entities': [{'offset': 0, 'length': 6, 'type': 'bot_command'}]}},
+     {'update_id': 389398453, 'message': 
+     {'message_id': 137, 'from': 
+     {'id': 1209619850, 'is_bot': False, 'first_name': 
+     "To'ychiyev Abdulbosit (عبد الباسط)", 'username': 'abdulbosit_tatu', 
+     'language_code': 'en'}, 'chat': {'id': 1209619850, 'first_name': 
+     "To'ychiyev Abdulbosit (عبد الباسط)", 'username': 'abdulbosit_tatu',
+      'type': 'private'}, 'date': 1703346082, 'text': 'hello'}}]}
 
 """
+
