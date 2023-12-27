c57a4794f1712b492f9b93a4cb738c829f522348
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-27 12:30:12 +0500

    Matplotlib getting start and install, __version__ checking

---
+++ pw3school/python_matplotlib/getting_started.py
@@ -0,0 +1,18 @@
+# Installation Matplotlib
+from pip._internal.distributions import installed
+
+# If you have Python and PIP already installed on a system, then installation of Matplotlib is very easy
+# Install it using this command
+# pip install matplotlib
+
+# Import Matplotlib
+# Once Matplotlib is installed, import it in your applications by adding the import module statement
+
+import matplotlib
+
+# Now Matplotlib is imported and ready to use
+# Checking Matplotlib version
+# The version string is stored under __version__ attribute
+print(matplotlib.__version__)  # 3.8.2
+# Note: two underscore characters  are used in __version__.
+
