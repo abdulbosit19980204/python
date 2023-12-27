3738b967603543e581a28b9ba763776837e423b1
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-27 12:23:52 +0500

    Matplotlib Intro

---
+++ pw3school/python_matplotlib/intro.py
@@ -0,0 +1,8 @@
+# Matplotlib tutorial
+
+# What is the matplotlib ?
+
+# Matplotlib is a low level graph plotting library in python that serves as a visualization utility
+# Matplotlib was created by John D. Hunter
+# Where is the Matplotlib Codebase?
+# The source code for Matplotlib is located at this gitHub repository https://github.com/matplotlib/matplotlib
