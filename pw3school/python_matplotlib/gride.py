aacec4f9ab7d6082bd8c856d91bbdaaf23acf5de
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-28 10:45:01 +0500

    Grid

---
+++ pw3school/python_matplotlib/gride.py
@@ -0,0 +1,34 @@
+# Add Grid Lines to a Plot
+
+# With PyPlot, you can use the grid() function to add grid lines to the plot
+# Add grid lines to the plot
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+plt.plot(x, y)
+plt.grid()
+plt.show()
+
+# specify Which Grid Lines to Display
+# You can use the axis parameter in the grid() function to specify which gird lines  to display.
+# Legal values are: 'x','y' and both. Default value 'both'.
+plt.plot(x, y)
+plt.grid(axis='x')
+plt.show()
+
+plt.plot(x, y)
+plt.grid(axis='y')
+plt.show()
+
+# Set Line Properties for the Grid
+# You can also set the line properties of the grid, like this.
+plt.plot(x,y)
+plt.grid(color='red', linestyle='--', linewidth=2)
+plt.show()
