cba71cdbfa2e131937f126e02b484683a7562501
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-01-04 16:38:06 +0500

    MongoDB det started

---
+++ pw3school/python_mongodb/get_started.py
@@ -0,0 +1,26 @@
+# Python MongoDB
+# Python can be used in database applications
+# One of the most popular NoSQL database is MongoDB
+
+# MongoDB
+
+# MongoDB Stores data in JSON-like documents, which makes the database very flexible and scalable
+# To be able to experiment with the code examples in this tutorial you will need access to a MongoDB database
+
+# PyMongo
+# Python needs a MongoDB driver to access the MongoDB database
+# In this tutorial we will use the MongoDB driver "PyMongo"
+# Recommended that you use pip to install "PyMongo"
+# PIP is most likely already installed in your Python enviroment
+# Navigate your command line to the location of PIP, and type the following
+
+# Download and install "PyMongo": python -m pip install pymongo
+
+# Now you have downloaded and installed a mongodb driver
+
+# Test PyMongo
+# To test if the installation was successful, or if you already have "pymongo" installed, create a Python page
+# with the following content
+
+import pymongo
+# If the above code was execude with no errors, pymongo installed and ready to be used
--- pw3school/python_mysql/dskam.py
+++ pw3school/python_mysql/dskam.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import matplotlib.pyplot as plt
+import numpy as np
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -8,9 +10,22 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("Jimmy", "China town")
-mycursor.execute(sql, val)
-mydb.commit()
-
-print(mycursor.rowcount, " data added, ID: ", mycursor.lastrowid)
+sql = "SELECT name FROM customers"
+sql2 = "SELECT id FROM customers"
+mycursor.execute(sql)
+result = mycursor.fetchall()
+mycursor.execute(sql2)
+ids = mycursor.fetchall()
+print(ids)
+# plt.bar(ids, result)
+# # plt.title(result)
+# plt.show()
+x = []
+t=[]
+for i in ids:
+    x.append(int(list(i)[0]))
+for j in result:
+    t.append(str(list(j)))
+print(t)
+plt.bar(x,t)
+plt.show()
