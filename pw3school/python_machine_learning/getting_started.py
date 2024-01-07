e5a010f0f66acd20161fbdbae1077c2e06601b1f
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-01-07 05:21:37 +0500

    Machine Learning getting started

---
+++ pw3school/python_machine_learning/getting_started.py
@@ -0,0 +1,35 @@
+# Machina Learning
+
+# Machine Learning is a program that analyses data and learns to predict the outcome.
+# Where to start
+# In this tutorial we will go back to mathematics and study statistics, and how to calculate important numbers based on data sets
+# We will also learn how to use various Python modules to get the answer we need
+# And we will learn how to make function that are able to predict the outcome based on what we have learned
+
+# Data Set
+# In the mind of a computer, a data set is any collection of data. It can be anything from an array to a complete database
+# Example of an array
+# [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+# Data Types
+# To analayze data, it is important to know what type of data we are dealing with
+# We can split the data types into three main categories
+
+# Numerical
+# Categorical
+# Ordinal
+
+# Numerical data are numbers, and can be split into two numerical categories
+# Discrete Data
+#  -counted data that are limited to integres. Example: The number of cars passing by
+# Continues Data
+#  -measured data that can be any number. Example: The price of an item, or the size of an item
+
+# Categorical Data are values that cannot be measured up againist each other. Example:  color value, or any yes/no values
+
+# Ordinal data are like categorical data, but can be measured up againist each other, Example: shcool grades where A is
+# better than B, and so on
+
+# By knowing the data type of your data source, you will be able to know what technique to use when analayzing them.
+# You will learn more about statistics and analyzing data in the next chapters
+
