86b85893d813d13ad5b41108ce0a716ae89ac2a7
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-01-09 20:03:41 +0500

    calss iterpolatsiya

---
+++ ba.uz/14-dars/class_additional.py
@@ -0,0 +1,27 @@
+class Car:
+    def __init__(self, name, color, price):
+        self.name = name
+        self.color = color
+        self._price = price
+
+    def __str__(self):
+        return f"Name: {self.name}, Color: {self.color}"
+
+
+class NewCar(Car):
+    def __init__(self, name, color, price, year, km):
+        super().__init__(name, color, price)
+        self.__year = year
+        self._km = km
+        self.price = price
+
+    def __str__(self):
+        return f"Name: {self.name}, Color: {self.color}, Price:{self._price} , Year: {self.__year}, KM: {self._km}"
+
+
+car1 = NewCar("Nexi", "white", 11000, 2000, 200000)
+print(car1)
+car1.year = 2022
+car1.km = 1000
+car1.price = 41000
+print(car1)
--- pw3school/python_machine_learning/linear_regression.py
+++ pw3school/python_machine_learning/linear_regression.py
@@ -1,3 +1,6 @@
 # Machine Learning - Linear Regression
 
 # Regression
+# The term  regression is used when you try to find the relationship between variables
+# In Machine Learning, and in statistical modeling, that relationship is used to predict the outcome of future events
+
