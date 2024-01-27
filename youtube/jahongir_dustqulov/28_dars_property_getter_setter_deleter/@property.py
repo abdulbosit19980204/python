33fb8aaf06d8db8190c45952ad0a0a47fbec47a4
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-01-27 19:09:05 +0500

    property getter and setter

--- youtube/jahongir_dustqulov/28_dars_property_getter_setter_deleter/@property.py
+++ youtube/jahongir_dustqulov/28_dars_property_getter_setter_deleter/@property.py
@@ -1,10 +1,24 @@
-from functools import reduce
+class Car:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        self.petrol = 1
+
+    def get_info(self):
+        print(f"Name: {self.name}\nColor: {self.color}")
+
+    def set_color(self, color):
+        self.color = color
+
+    def set_petrol(self, petrol):
+        self.petrol = petrol
+
+    @property
+    def rasxod(self):
+        return 100 / self.petrol
 
 
-def func(x, y):
-    return x * y
-
-
-lst = [int(i) for i in input().split()]  # 1 2 3 5
-
-print(reduce(func, lst))
+nexia = Car("Nexia", "grey")
+nexia.get_info()
+nexia.set_petrol(4)
+print(nexia.rasxod)
