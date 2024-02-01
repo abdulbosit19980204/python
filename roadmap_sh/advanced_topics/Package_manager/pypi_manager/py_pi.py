dd6b478ef96bfca2fcf7ab23eb90cf3de5a88659
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-01 17:00:45 +0500

    pypi

---
+++ roadmap_sh/advanced_topics/Package_manager/pypi_manager/py_pi.py
@@ -0,0 +1,6 @@
+# PyPi
+
+# PyPi typically pronounced pie-pee-eye, is a repository containing several hundred thousands packages.
+# These range from trivial Hello, World implementations to advanced deep learning libraries.
+
+# Offical website: pypi.org
