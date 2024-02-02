ca0d7f4758c6acb660ac12bab42418e3bda54287
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-02 18:13:45 +0500

    conda manager

---
+++ roadmap_sh/advanced_topics/Package_manager/conda_manager.py
@@ -0,0 +1,8 @@
+# Conda
+
+# Conda is an open source package manager and it helps you find and install packages
+
+# If you need a package that requires a different version of Python, you do not need to switch to different enviroment
+# manager, becouse conda is also  an environment manager. With just few commands, you can  set up a totally  seperate
+# environment to run that different version of Python, while contuining to run your usual version of Python in your
+# normal environment
