80010a0ab6e8c3b24d2e68369d88db46c402ea29
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-10 12:05:15 +0500

    Django project create

---
+++ django/pw3school/django_create_project/django_project_create_intro.py
@@ -0,0 +1,42 @@
+# Django Create Project
+
+# My First Project
+# Once you have come up with a suitable name for your Django project, like min: my_tennis_club,
+# navigate to where in the file system you want to store the code (in the virtual environment),
+# I will navigate to the myworld folder, and run this command prompt:
+
+# django_admin startproject my_tennis_club
+
+# Django creates a my_tennis_club folder on my computer, with this content:
+
+"""
+my_tennis_club
+    manage.py
+    my_tennis_club/
+        __init__.py
+        asgi.py
+        setting.py
+        urls.py
+        wsgi.py
+"""
+
+# These are all files and folders with a specific meaning, you will learn about some of them later in this tutorial,
+# but for now, it is more important to know that this is the location of your project,
+# and that you can start building applications int it.
+
+# Run the Django Project
+
+# Now that you have a Django project, you can run it, and see what it looks like in a browser.
+# Navigate to the /my_tennis_club folder and execute this command in the command prompt:
+
+# py manage.py runserver
+
+# Open a new browser window and type 127.0.0.1:8000 in the address bar
+# The result message showed and run it  if all them correct
+
+
+# What's Next?
+
+# The next step is to make an app in your project.
+# You cannot have a web page created with Django without an app.
+
