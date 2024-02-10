fe75ccc12c6a1da102f623277b1e1c3226676b5d
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-10 10:26:07 +0500

    install django

---
+++ django/pw3school/install_django/install_django_instruction.py
@@ -0,0 +1,19 @@
+# Install Django
+# Now that we have created a virtual environment, we are ready to install Django.
+
+# Note: Remember to install Django while you are in the virtual environment!
+
+# Django us installed using pip, with this command:
+# Windows
+# py -m pip install Django
+
+# Unix/MacOS:
+# python -m pip install Django
+
+
+# Check Django Version
+# You can check if Djnago is installed by asking for its version number like this:
+# django-admin --version
+
+# If Django is installed, you will get a result with the version number:
+# Our django version is 5.0.2
