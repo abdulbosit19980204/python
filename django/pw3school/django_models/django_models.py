5f75dabe420546c65374183b50588233aadb4592
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-12 05:01:17 +0500

    Django Models

---
+++ django/pw3school/django_models/django_models.py
@@ -0,0 +1,12 @@
+# Django Models
+
+# A Django model is a table in your database
+
+# Django Models
+
+# Up until now in this tutorial, output has been static data from Python on HTML templates
+
+# Now we will see how Django allows us to work with data, without having to change or upload files in the process.
+
+# In Django, data is created in objects, called Models, and is actually tables in a database.
+
