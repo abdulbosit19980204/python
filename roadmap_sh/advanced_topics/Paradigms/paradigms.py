95534750740285124a958f38a8c3284b3945d8ae
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-14 18:45:16 +0500

    paradigms

---
+++ roadmap_sh/advanced_topics/Paradigms/paradigms.py
@@ -0,0 +1,24 @@
+# Python Paradigms
+
+# Python is a multi-paradigm programming language, which means that it supports several programming paradigms.
+# Some of the main paradigms supported by Python  are:
+"""
+    Imperative programming: This paradigm focuses on telling the computer what to do, step by step.
+    Python supports imperative programming with features such as variables, loops, and control structures.
+
+    Object-oriented programming(OOP): This paradigm is based on the ides of objects and their interactions.
+    Python supports OOP with features such as classes, inheritance, and polymorphism.
+
+    Functional programming: This paradigm is based on the idea of functions as first-class citizens,
+    and it emphasizes the use of pure functions and immutable data. Python supports functional programming with
+    features such as higher-order functions, lambda expressions, and generators.
+
+    Aspect-oriented programming: This paradigm is based on the idea of separating cross-cutting concerns from the main
+    functionality of a programming language. Python doesn't have built-in support for aspect-oriented programming,
+    but it can be achieved using libraries or languages extensions.
+"""
+
+# Python's support for multiple paradigms makes it a versatile and flexible language,
+# and it allows developers to choose the paradigm that best fits their needs
+
+
---
+++ roadmap_sh/advanced_topics/Paradigms/programming_paradigms.py
@@ -0,0 +1,47 @@
+# Programming Paradigms in Python
+
+"""
+Paradigm can also be termed as a method to solve some problems or do some tasks.
+A programming paradigm is an approach to solve the problem using some programming language, or also we can say it is
+a method to solve a problem using tools and techniques that are available to us following some approach.
+There are lots of programming languages that are known but all of them need to follow some strategy
+when they are implemented and this methodology/strategy is paradigms.
+"""
+
+# Python supports three types of Programming paradigms
+
+"""
+    Object Oriented programming 
+    Procedure Oriented programming
+    Functional programming paradigms
+"""
+
+# Object Oriented programming paradigms
+
+"""
+In the object-oriented programming paradigm, objects are the key element of paradigms. Objects can simply be defined as
+the instance of a class that contains both data members and the method functions. Moreover, the object-oriented style
+relates data members and methods functions that support encapsulation and with the help of the concept of an inheritance,
+the code can be easily reusable but the "major disadvantage" of object-oriented programming paradigm is that if the code is 
+not written properly then the program becomes a monster.
+
+Advantages
+
+    Relation with Real world entities
+    Code reusability
+    Abstraction or data hiding
+    
+Disadvantages
+    
+    Data protection
+    Not suitable for all types problems
+    Slow speed
+
+"""
+
+
+# Procedural programming oaradigms
+
+"""
+
+"""
