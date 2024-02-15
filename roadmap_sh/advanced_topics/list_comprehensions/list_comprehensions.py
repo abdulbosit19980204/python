2205d6b008ca08797d6cbed06961831258aae516
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-15 19:27:18 +0500

    ...

---
+++ roadmap_sh/advanced_topics/list_comprehensions/list_comprehensions.py
@@ -0,0 +1,6 @@
+# List Comprehensions
+
+# List comprehensions are a concise way to create a list using a single line of code in Python. They are a powerful tool
+# for creating and manipulating lists, and they can be used to simplify and shorten code.
+
+fruits = ["apple", ]
