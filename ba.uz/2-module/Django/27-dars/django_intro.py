b8f634a7882288b76e848a1fb3f0a28d13e83a20
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-17 15:45:35 +0500

    27-dars

---
+++ ba.uz/2-module/Django/27-dars/django_intro.py
@@ -0,0 +1,34 @@
+# 1. Create virtual environment
+#     py -m venv venv
+import django
+
+# 2. Activate virtual environment
+#     2.1 on Linux
+#             source venv/bin/activate
+#     2.2 on Windows
+#             ./venv/Scripts/activate
+
+# 3. Install Django
+# After, activate of virtual environment we can install Django for Our project
+# pip install django --- on Windows
+# pip3 install django --- on Linux
+
+# 4. Start Project
+#     To start a new project we need to write "django-admin" to run django commands
+#   django-admin --version
+#   django-admin startproject kunuz
+
+# 5. Run the project
+#     To run our Django project:
+#     py ./manage.py runserver -- windows
+#     python3 ./manage.py runserver -- linux
+
+# 6. Open the project that running
+# http: // 127.0.0.1: 8000 / -- this link is running on this address
+
+# 7. Stop the server
+#     to stop the Server press ctrl + c
+
+# 8. views
+
+# this file get request from user
