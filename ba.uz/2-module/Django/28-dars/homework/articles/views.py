7b65a7a73a55bee80c6bec37ba5615ca63c58d96
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-20 19:16:35 +0500

    Article sorted by date

--- ba.uz/2-module/Django/28-dars/homework/articles/views.py
+++ ba.uz/2-module/Django/28-dars/homework/articles/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render, HttpResponse
 
 from .models import Articles
 
-articles = Articles.objects.all()
-
 
 def home_view(request):
     return render(request, "home.html")
 
 
 def articles_view(request):
+    articles = Articles.objects.all()
     d = {
-        "articles": articles,
+        "articles": articles[::-1],
         "message": "Articles are loading...",
     }
     return render(request, "articles.html", context=d)
 
 
 def article_views(request, article_id):
+    articles = Articles.objects.all()
     d = {
         "article": articles[article_id - 1],
         "message": "Article send to front",
