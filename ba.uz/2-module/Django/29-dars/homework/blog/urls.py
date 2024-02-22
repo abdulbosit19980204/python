b597bea2086ffb9f66f5646dfedcc0dc70f0d46f
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-22 17:15:54 +0500

    29-dars Homework

--- ba.uz/2-module/Django/29-dars/homework/blog/urls.py
+++ ba.uz/2-module/Django/29-dars/homework/blog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from .views import home_view, about_view, contact_view, blog_view
+
 urlpatterns = [
-    path
+    path('', home_view),
+    path('blog/', blog_view),
+    path('about/', about_view),
+    path('contact', contact_view),
 ]
--- ba.uz/2-module/Django/29-dars/homework/blog/views.py
+++ ba.uz/2-module/Django/29-dars/homework/blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'index.html')
+
+
+def blog_view(request):
+    return render(request, 'blog.html')
+
+
+def about_view(request):
+    return render(request, 'about.html')
+
+
+def contact_view(request):
+    return render(request, 'contact.html')
