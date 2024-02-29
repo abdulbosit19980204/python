c0923cff8119f0932dff172b2bd9f8306a190ade
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-02-29 08:36:56 +0500

    Homework Done

--- ba.uz/2-module/Django/32-dars/homework/blog/admin.py
+++ ba.uz/2-module/Django/32-dars/homework/blog/admin.py
@@ -22,7 +22,7 @@ class CommentsAdmin(admin.ModelAdmin):
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'created_at')
+    list_display = ('id', 'name', 'created_at', 'updated_at')
     list_display_links = ('id', 'name')
 
 
