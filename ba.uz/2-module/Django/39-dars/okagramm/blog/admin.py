1ec8f088bf23cb9236b20105dac7b388c88d0da2
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-03-23 02:07:03 +0500

    Admin page showed images from post on Post section

--- ba.uz/2-module/Django/39-dars/okagramm/blog/admin.py
+++ ba.uz/2-module/Django/39-dars/okagramm/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import MyUser, FollowMyUser, Post, CommentPost, LikePost, Notification
+from django.utils.html import format_html
 
 
 # Register your models here.
@@ -12,11 +13,30 @@ class MyUserAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'like_count', 'write_comment', 'created_at')
+    def image_tag(self, obj):
+        return format_html('<img src="{}" style="max-width:100px; max-height:100px"/>'.format(obj.post_image.url))
+
+    image_tag.short_description = 'Image'
+
+    list_display = ('id', 'image_tag', 'author', 'like_count', 'write_comment', 'created_at',)
     list_display_links = ('id', 'author', 'like_count')
 
 
-admin.site.register(FollowMyUser)
-admin.site.register(CommentPost)
+@admin.register(FollowMyUser)
+class FollowMyUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'follower', 'following', 'created_at')
+    list_display_links = ('id', 'follower', 'following', 'created_at')
+
+
+@admin.register(CommentPost)
+class CommentPostAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="{}" style="max-width:100px; max-height:100px"/>'.format(obj.post.post_image.url))
+
+    image_tag.short_description = 'Image'
+    list_display = ('id', 'image_tag', 'message', 'author', "created_at")
+    list_display_links = ('id', 'image_tag', 'message', 'author', )
+
+
 admin.site.register(LikePost)
 admin.site.register(Notification)
