56be546b2fe4815439adc22e78c37e442cab9926
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-03-25 16:44:32 +0500

    AWS account

---
+++ django/pw3school/Django_PostgreSQL/create_aws_account.py
@@ -0,0 +1,13 @@
+# Create AWS account
+
+# Why AWS?
+
+# There are many providers out there that can host Django projects and PostgreSQL databases.
+# In this tutorial we will use the Amazon Web Services (AWS) platform, mainly because they offer a free solution that can
+# host both Django projects and PostgreSQL databases. All you need is an AWS account.
+
+# Add the RDS Service
+# Once you have an AWS account, you can start creating a database.
+# We will use a database service at AWS called RDS.
+
+# in the search field, search for "RDS", and click to start the service:
