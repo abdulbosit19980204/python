520f7d30a20cf886ae9487253cc4f24cbc57bd2c
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-03-25 16:56:15 +0500

    Create database in RDS

---
+++ django/pw3school/Django_PostgreSQL/create_database_in_RDS.py
@@ -0,0 +1,12 @@
+# Create Database
+
+# Inside the RDS service, create a database, either by navigating to the Database section, or just click the
+# "Create database" button:
+
+
+# Settings
+
+# Once you have started creating a database, you will be given some choices for the type and settings of your database.
+# To add a PostgreSQL database to your Django project, choose the following options:
+
+# Standard creation method:
