93162ecc59b1fe76e6947d446a0b5601b009e71b
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-03-25 17:51:50 +0500

    Add members

---
+++ django/pw3school/Django_PostgreSQL/add_members.py
@@ -0,0 +1,16 @@
+# PostgreSQL - Add Members
+
+# Members
+# The "My Tennis Club" project has no members: 127.0.0.1:8000
+# That is because we have created a brand new database, and it is empty
+# The old SQLite database contained 5 members, so let us dive into the admin interface and add the same 5 members
+
+# But first we have to create a new superuser.
+
+# Create superuser
+# Since we now have a new database, we have to create the superuser once again:
+# This is done by typing this command in the command view
+
+# py manage.py createsuperuser
+
+# which will  give this prompt
