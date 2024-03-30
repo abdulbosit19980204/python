dc38b8f6ccb362dcd3eb94dc81ee1fb3c1da146a
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-03-30 13:09:38 +0500

    Fetch data

---
+++ sql/p_sql_w3shool/database/fetch_data.sql
@@ -0,0 +1,15 @@
+-- PostgreSQL Select Data
+
+-- To retrieve data from a data base, we use the "SELECT" statement.
+
+-- Specify Columns
+
+-- By specifying the column names, we can choose which columns to select.
+SELECT barnd, year
+FROM cars;
+
+-- Return All Columns
+-- Specify a * instead of the column names to select all columns:
+
+SELECT *
+FROM cars;
