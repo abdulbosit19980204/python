f9ad6734d3404ff1d0aa971329a797f09e011df9
unknown <C:\Users\BlackTiger\AppData\Roaming\The Bat!>
2024-04-01 14:20:43 +0500

    TEST connection

--- sql/p_sql_w3shool/database/alter_column.sql
+++ sql/p_sql_w3shool/database/alter_column.sql
@@ -5,3 +5,4 @@
 -- To change the data type, or the size of table column we have to use the ALTER TABLE statement.
 -- The ALTER TABLE statement is used to add, delete, or modify columns in an existing table.
 -- The ALTER TABLE statement is also used to add and drop various constraints on an existing table.
+
