89783a6d9ef3f74c7373cfc0b8abd66573581192
unknown <C:\Users\BlackTiger\AppData\Roaming\The Bat!>
2024-04-02 09:09:49 +0500

    Alter tabale and alter column

--- sql/p_sql_w3shool/database/alter_column.sql
+++ sql/p_sql_w3shool/database/alter_column.sql
@@ -6,3 +6,27 @@
 -- The ALTER TABLE statement is used to add, delete, or modify columns in an existing table.
 -- The ALTER TABLE statement is also used to add and drop various constraints on an existing table.
 
+
+-- ALTER COLUMN
+
+-- We want to change the data type of the year column of the cars table from int to varchar(4).
+-- To modify a column, use the ALTER COLUMN statement and the TYPE keyword followed by the data type:
+
+-- Example
+-- Change the year column from INT to VARCHAR(4)
+ALTER TABLE cars
+    ALTER COLUMN year TYPE VARCHAR(4);
+
+-- Note: Some data types cannot be converted if the column has value E.g. numbers can always be converted to text, but
+-- text cannot always be converted to numbers
+
+-- Change Maximum Allowed Characters
+
+-- We also want to change the maximum number of characters allowed in the color column of the cars table.
+-- Use the same syntax as above, use the ALTER COLUMN  statement and the TYPE keyword followed by the new data type
+
+-- Example
+-- Change the color column from VARCHAR(255) to VARCHAR(30).
+
+ALTER TABLE cars
+    ALTER COLUMN color TYPE VARCHAR(30);
