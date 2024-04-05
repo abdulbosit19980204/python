c8b20fecb66a4cb30119188b8e8f5c1e0d84e6c4
unknown <C:\Users\BlackTiger\AppData\Roaming\The Bat!>
2024-04-05 08:52:07 +0500

    SUM Function

---
+++ sql/p_sql_w3shool/sql_syntax/sum.sql
@@ -0,0 +1,13 @@
+--     PostgreSQL SUM Function
+
+-- SUM
+
+-- The SUM() Function returns the total sum of a numeric column.
+-- The following SQL statement finds the sum of the quantity fields in the order_details table:
+
+-- Example
+-- Return the total amount of ordered items
+SELECT SUM(quantity)
+FROM order_details;
+
+-- Note: NULL values are ignored.
