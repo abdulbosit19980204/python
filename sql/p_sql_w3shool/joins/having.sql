86f471b7cb76666fc02079df6e4ed74b33df63d6
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-04-09 09:58:36 +0500

    Having

--- sql/p_sql_w3shool/joins/having.sql
+++ sql/p_sql_w3shool/joins/having.sql
@@ -1,3 +1,15 @@
 -- PostgreSQL HAVING Clause
 
 -- Having
+
+-- The HAVING clause was added to SQL because the WHERE clause cannot be used with aggregate functions.
+-- Aggregate function are often used with GROUP BY clauses, and by adding HAVING we can write condition like we do with
+-- WHERE clause
+
+-- Example
+-- List only countries that are represented more than 5 times:
+
+SELECT COUNT(customer_id), country
+FROM customers
+GROUP BY country
+HAVING COUNT(customer_id) > 5;
