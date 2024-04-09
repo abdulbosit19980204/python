afe70822cd85ad4f42af9d3fac9896eb43d4c002
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-04-09 10:19:41 +0500

    Having

--- sql/p_sql_w3shool/joins/having.sql
+++ sql/p_sql_w3shool/joins/having.sql
@@ -13,3 +13,22 @@ SELECT COUNT(customer_id), country
 FROM customers
 GROUP BY country
 HAVING COUNT(customer_id) > 5;
+
+
+-- More HAVING examples
+-- The following SQL statement lists only orders with a total price of 400$ or more:
+SELECT order_details.order_id, SUM(products.price)
+FROM order_details
+         LEFT JOIN products ON order_details.product_id = products.product_id
+GROUP BY order_id
+HAVING SUM(products.price) > 400.00;
+
+-- Lists customers that have ordered for 1000$ or more:
+-- Example
+SELECT customers.customer_name, SUM(products.price)
+FROM order_details
+         LEFT JOIN products ON order_details.product_id = products.product_id
+         LEFT JOIN orders ON order_details.order_id = orders.order_id
+         LEFT JOIN customers ON orders.customer_id = customers.customer_id
+GROUP BY customer_name
+HAVING SUM(products.price) > 1000.00;
