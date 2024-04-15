313e19900317f2b6f9e8bdfeafe0ddc3f295c94b
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-04-15 10:32:01 +0500

    DELETE AND UPDATE ORDER BY LIMIT

--- ba.uz/2-module/SQL/intro.sql
+++ ba.uz/2-module/SQL/intro.sql
@@ -18,3 +18,53 @@ ORDER BY title
 
 DROP TABLE post;
 
+SELECT *
+FROM post
+WHERE title = 'A'
+ORDER BY id DESC
+LIMIT 1;
+
+
+UPDATE post
+SET title='C'
+WHERE id = 3;
+
+SELECT *
+FROM post
+WHERE id = 3;
+
+
+SELECT *
+FROM post
+ORDER BY id;
+
+DELETE
+FROM post
+WHERE id = 24;
+
+CREATE TABLE student
+(
+    id         serial primary key,
+    name       VARCHAR(255),
+    age        INT,
+    university VARCHAR(255)
+);
+
+
+INSERT INTO student (name, age, university)
+VALUES ('Abdulbosit', 26, 'TUIT'),
+       ('Diyorbek', 20, 'NamDU'),
+       ('Shokirali', 36, 'MGU');
+
+SELECT *
+FROM student;
+
+SELECT *
+FROM student
+WHERE age = (SELECT MAX(age) FROM student);
+
+SELECT *
+FROM student
+ORDER BY age DESC
+LIMIT 1;
+
