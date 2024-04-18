b14ee06b4e9b5191fee280e26e561315f5f7bb8d
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-04-18 14:26:08 +0500

    Async and await

---
+++ ba.uz/2-module/FastAPI/16.04.2024/main.py
@@ -0,0 +1,140 @@
+from fastapi import FastAPI
+import uvicorn
+
+posts = [
+    {
+        "id": 1,
+        "title": "O'zbekiston",
+        "description": "O'zbekiston kelajagi buyuk davlat",
+        "author_id": 2,
+    },
+    {
+        "id": 2,
+        "title": "China",
+        "description": "China is the country that has the most populous",
+        "author_id": 1,
+    },
+    {
+        "id": 3,
+        "title": "O'zbekiston2",
+        "description": "O'zbekiston kelajagi buyuk davlat",
+        "author_id": 3,
+    },
+    {
+        "id": 4,
+        "title": "O'zbekiston3",
+        "description": "O'zbekiston kelajagi buyuk davlat",
+        "author_id": 1,
+    }
+]
+
+comments = [
+    {
+        "id": 1,
+        "message": "zo'r",
+        "post_id": 2
+    },
+    {
+        "id": 1,
+        "message": "Ajoyib",
+        "post_id": 1
+    },
+    {
+        "id": 1,
+        "message": "Wav",
+        "post_id": 2
+    },
+    {
+        "id": 1,
+        "message": "Zerikarli",
+        "post_id": 3
+    },
+    {
+        "id": 1,
+        "message": "zo'r",
+        "post_id": 4
+    },
+    {
+        "id": 1,
+        "message": "zo'r",
+        "post_id": 1
+    },
+    {
+        "id": 1,
+        "message": "zo'r",
+        "post_id": 3
+    },
+]
+
+authors = [
+    {
+        "id": 1,
+        "name": "Admin",
+    },
+    {
+        "id": 2,
+        "name": "Kimdur",
+    },
+    {
+        "id": 3,
+        "name": "Bratan",
+    }
+]
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {'Hello': 'World'}
+
+
+@app.get('/posts')
+async def get_posts():
+    return {"posts": posts}
+
+
+async def get_post(pk: int):
+    for post in posts:
+        if post['id'] == pk:
+            return post
+
+
+async def get_author(user_id: int):
+    for author in authors:
+        if author['id'] == user_id:
+            return author
+
+
+async def get_comments(post_id: int):
+    post_comments = []
+    for comment in comments:
+        if comment['post_id'] == post_id:
+            post_comments.append(comment)
+    return post_comments
+
+
+# @app.get('/article/{pk}')
+# async def get_post(pk: int):
+#     for post in posts:
+#         if post['id'] == pk:
+#             aid = post['author_id']
+#             post_comments = []
+#             for comment in comments:
+#                 if comment['post_id'] == post['id']:
+#                     post_comments.append(comment)
+#             for author in authors:
+#                 if author['id'] == aid:
+#                     return {"post": post, "author": author, "comments": post_comments}
+#     return {"message": "Post Not Found"}
+
+
+@app.get('/posts/{pk}')
+async def get_posts(pk: int):
+    post = await get_post(pk)
+    author = await get_author(post['author_id'])
+    post_comments = await get_comments(post['id'])
+    return {"post": post, "author": author, "comments": post_comments}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
--- ba.uz/2-module/FastAPI/first_step.py
+++ ba.uz/2-module/FastAPI/first_step.py
@@ -10,6 +10,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 # Run the live server:
 # uvicorn main:app --reload
 
@@ -20,3 +21,75 @@ async def root():
 # -- reload: make the server restart after code changes. Only use for development
 
 # In the output, there is a line with something like:
+# INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
+
+# That line shows the URL where your app is being served, in your local machine
+
+# Check it
+
+# Open your browser at http://127.0.0.1:8000/
+# You will see the JSON response
+# {"message": "Hello World"}
+
+
+# Interactive API docs
+# Now go to http://127.0.0.1:8000/docs
+
+# Alternative API docs
+# And now, go to http://127.0.0.1:8000/redoc
+
+# OpenAPI
+# FastAPI generates a "schema" with all your API using the OpenAPI standart for defining APIs.
+
+# "Schema"
+
+# A "schema" is a definition or description of something. Not the code that implements it,
+# but just an abstract description
+
+# API "schema"
+# In this case, OpenAPI is a specification that dictates how to define a schema of your API.
+# This schema definition includes your API paths, the possible parameters they take, etc.
+
+# Data "schema"
+# The term "schema" might also refer to the shape of some data, like a JSON content.
+# In that case, it would mean the JSON attributes, and data types they have, etc.
+
+# OpenAPI and JSON schema
+# OpenAPI defines an API schema for your API. ANd that schema includes definitions (or "schemas") of the data sent
+# and received by your API using JSON Schema, the standard for JSON data schema.
+
+# Check the openapi.json
+# If you are curious about how the raw OpenAPI schema looks like, FastAPI automatically generates a JSON (schema) with
+# the description of all your API.
+# You can see it directly at: http://127.0.0.1:8000/openapi.json
+# It will show a JSON starting with something like:
+
+"""
+{
+    "openapi": "3.1.0",
+    "info": {
+        "title": "FastAPI",
+        "version": "0.1.0"
+    },
+    "paths": {
+        "/items/": {
+            "get": {
+                "responses": {
+                    "200": {
+                        "description": "Successful Response",
+                        "content": {
+                            "application/json": {
+
+
+
+...
+"""
+
+# What is OpenAPI
+
+# The OpenAPI schema is what powers the two interactive documentation systems included.
+# And there are doznes of alternatives, all based on OpenAPI. You could easily add any of those alternatives to
+# your application built with FastAPI
+
+# You could also use it to generate code automatically, for clients that communicate with your API.
+# For example, frontend, mobile or IoT applications
