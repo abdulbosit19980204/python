8c96144ee29378545b1fab25cdcf31f76866ffca
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-04-18 11:02:37 +0500

    Async and await

--- ba.uz/2-module/FastAPI/first_step.py
+++ ba.uz/2-module/FastAPI/first_step.py
@@ -1,3 +1,22 @@
 # First Step
 
 # The simplest FastAPI file could look like this:
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Run the live server:
+# uvicorn main:app --reload
+
+# Note:
+# The command uvicorn main:app refers to
+# main: the file main.py (the Python "module")
+# app: the object created inside of main.py with the line app=FastAPI()
+# -- reload: make the server restart after code changes. Only use for development
+
+# In the output, there is a line with something like:
