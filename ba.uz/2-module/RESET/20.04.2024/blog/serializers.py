68540da6a994908aab60576705bc5e23a3a8f6ae
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-04-25 17:38:37 +0500

    Serialaizer

--- ba.uz/2-module/RESET/20.04.2024/blog/serializers.py
+++ ba.uz/2-module/RESET/20.04.2024/blog/serializers.py
@@ -8,11 +8,21 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['posts'] = PostSerializer(Post.objects.filter(category_id=instance.id), many=True).data
+        return data
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['comments'] = CommentSerializer(Comment.objects.filter(post_id=instance.id), many=True).data
+        return data
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -25,3 +35,8 @@ class MyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['posts'] = PostSerializer(Post.objects.filter(author__id=instance.id), many=True).data
+        return data
--- ba.uz/2-module/RESET/20.04.2024/blog/views.py
+++ ba.uz/2-module/RESET/20.04.2024/blog/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+from django.db.models import Q
+
 from .models import MyUser, Comment, Post, Category
 from .serializers import PostSerializer, MyUserSerializer, CommentSerializer, CategorySerializer
 
@@ -25,13 +27,9 @@ def category_view(request):
 @api_view(['GET'])
 def category_detail_view(request, category_id):
     cat = Category.objects.filter(id=category_id).first()
-    posts = Post.objects.filter(category_id=cat.id)
-
     categories = CategorySerializer(cat)
-    posts = PostSerializer(posts, many=True)
 
-    return_data = {'categories': categories.data, 'posts': posts.data}
-    return Response(return_data, status=status.HTTP_200_OK)
+    return Response(categories.data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'POST'])
@@ -43,16 +41,62 @@ def create_user_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        users = MyUser.objects.all()
+        serializer = MyUserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET', ])
+def posts_view(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def posts_view(request):
     if request.method == 'GET':
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    elif request.method == 'POST':
+        data = request.data
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PUT' or request.method == 'PATCH':
+        data = request.data
+        pk = request.GET.get('pk')
+        post = Post.objects.filter(id=pk, is_published=True).first()
+        serializer = PostSerializer(post, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # elif request.method == 'PATCH':
+    #     data = request.data
+    #     serializer = PostSerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        data = request.data
+        serializer = PostSerializer(data=data)
+        post = serializer
+        if serializer.is_valid():
+            del serializer
+            return Response(data={"data": post}, status=status.HTTP_200_OK)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -79,7 +123,7 @@ def search_view(request):
     if request.method == 'GET':
         keywords = request.query_params.get('keywords')
         if keywords:
-            posts = Post.objects.filter(title__contains=keywords)
+            posts = Post.objects.filter(Q(title__icontains=keywords) | Q(description__icontains=keywords))
             serializer = PostSerializer(posts, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
